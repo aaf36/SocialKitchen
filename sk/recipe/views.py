91e4b1a979f9397109c6f929757a5f1f8de234fd
Ali Faour <aaf36@mail.aub.edu>
2023-04-26 06:18:15 +0300

    added macronutrients calculating method to add recipe

--- sk/recipe/views.py
+++ sk/recipe/views.py
@@ -2,6 +2,8 @@ from django.forms import modelformset_factory
 from django.shortcuts import get_object_or_404, redirect, render, HttpResponse
 from django.contrib import messages
 from django.urls import reverse
+
+from macros.models import RecipeIngredientMacros, RecipeMacros
 from .forms import RecipeIngredientForm, RecipeForm
 from .models import Recipe, RecipeIngredient
 from macros.views import calc_macros, convert2grams
@@ -19,21 +21,42 @@ def home(request):
 
 def add_recipe(request):
     if not request.user.is_authenticated:
-        messages.success(request, "You Need To Login Before Accessing This Page...")
+        messages.success(request, "You need to login before accessing this page.")
         return redirect(reverse('user:login'))
     else:
         RecipeIngredientFormset = modelformset_factory(RecipeIngredient, form=RecipeIngredientForm, extra=0)
         if request.method == 'POST':
-            form =RecipeForm(request.POST, request.FILES)
+            form = RecipeForm(request.POST, request.FILES)
             formset = RecipeIngredientFormset(request.POST, queryset=RecipeIngredient.objects.none())
             if all([form.is_valid(), formset.is_valid()]):
-                recipe= form.save()
+                recipe = form.save()
                 ingredients = formset.save(commit=False)
+                total_calories = 0
+                total_protein = 0
+                total_carbs = 0
+                total_fats = 0
                 for ingredient in ingredients:
                     ingredient.recipe = recipe
                     ingredient.save()
-                return render(request, "recipe/details_recipe.html", {'recipe':recipe,'ingredients':ingredients})
-                
+                    ingredient_macros = RecipeIngredientMacros()
+                    ingredient_macros.recipe_ingredient = ingredient
+                    ingredient_macros.calories = calc_macros(ingredient.ingredient.calories, convert2grams(ingredient.unit, ingredient.quantity))
+                    ingredient_macros.protein = calc_macros(ingredient.ingredient.protein, convert2grams(ingredient.unit, ingredient.quantity))
+                    ingredient_macros.carbs = calc_macros(ingredient.ingredient.carbs, convert2grams(ingredient.unit, ingredient.quantity))
+                    ingredient_macros.fats = calc_macros(ingredient.ingredient.fats, convert2grams(ingredient.unit, ingredient.quantity))
+                    ingredient_macros.save()
+                    total_calories += ingredient_macros.calories
+                    total_protein += ingredient_macros.protein
+                    total_carbs += ingredient_macros.carbs
+                    total_fats += ingredient_macros.fats
+                recipe_macros = RecipeMacros()
+                recipe_macros.recipe = recipe
+                recipe_macros.calories = total_calories
+                recipe_macros.protein = total_protein
+                recipe_macros.carbs = total_carbs
+                recipe_macros.fats = total_fats
+                recipe_macros.save()
+                return render(request, "recipe/details_recipe.html", {'recipe':recipe,'ingredients':ingredients, 'macros':recipe_macros})
         else:
             form = RecipeForm()
             formset = RecipeIngredientFormset(queryset=RecipeIngredient.objects.none())
